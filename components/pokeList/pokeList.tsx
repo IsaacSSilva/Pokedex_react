662f3332ed801d7db6b8649e51757a4021e68f72
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-02-06 10:50:06 -0300

    🚧
    -melhoria no filtro
    -limitador de renderização

--- components/pokeList/pokeList.tsx
+++ components/pokeList/pokeList.tsx
@@ -3,9 +3,10 @@ import { useState, useEffect } from 'react'
 import { CardPokemon } from './pokeCard'
 
 export const PokedexList = () => {
-  const [addPoke, setAddPoke] = useState<number>(50)
-  const [valueNumero, setValuNumero] = useState()
+  const [addPoke, setAddPoke] = useState<number>(150)
+  const [pokeLimited, setPokeLimited] = useState<number>(50)
   const [valueName, setValueName] = useState<any>()
+  const [valueNumber, setValueNumber] = useState<any>()
   const [idPoke, setIdPoke] = useState([
     {
       data: {
@@ -16,8 +17,6 @@ export const PokedexList = () => {
       }
     }
   ])
-
-  const api = 'https://pokeapi.co/api/v2/pokemon?limit=50'
 
   useEffect(() => {
     getPoke()
@@ -37,21 +36,30 @@ export const PokedexList = () => {
 
   return (
     <>
-      <div className='flex m-auto justify-center items-center gap-7'>
+      <div className='m-auto flex flex-col justify-center items-center gap-2'>
         <input
-          placeholder='nome'
+          className='px-2 py-1 rounded-sm bg-opacity-75 bg-white focus:bg-opacity-100'
+          placeholder='Nome'
           type='text'
           onChange={e => {
             setValueName(e.target.value.toLowerCase())
           }}
         />
+
+        <input
+          className='px-2 py-1 rounded-sm bg-opacity-75 bg-white focus:bg-opacity-100'
+          placeholder='Numero'
+          type='text'
+          onChange={e => {
+            setValueNumber(e.target.value)
+          }}
+        />
       </div>
 
-      <div className='max-w-3xl flex flex-row flex-wrap m-auto py-10 px-1 md:p-5  justify-center items-center gap-5'>
-        {valueName
+      <div className='flex flex-row flex-wrap m-auto my-10 mx-1 md:m-5  justify-center items-center gap-5'>
+        {valueNumber
           ? idPoke
-              .filter(poke => poke.data.name.includes(valueName))
-
+              .filter(poke => poke.data.id == valueNumber)
               .map((poke, key) => (
                 <CardPokemon
                   key={key}
@@ -62,24 +70,53 @@ export const PokedexList = () => {
                   pokeType={poke.data.types[0].type.name}
                 />
               ))
-          : idPoke.map((poke, key) => (
-              <CardPokemon
-                key={key}
-                id={poke.data.id}
-                img={poke.data.sprites.front_default}
-                name={poke.data.name}
-                numero={poke.data.id.toString()}
-                pokeType={poke.data.types[0].type.name}
-              />
-            ))}
+          : valueName
+          ? idPoke
+              .filter(poke => poke.data.name.includes(valueName))
+              .map((poke, key) => (
+                <CardPokemon
+                  key={key}
+                  id={poke.data.id}
+                  img={poke.data.sprites.front_default}
+                  name={poke.data.name}
+                  numero={poke.data.id.toString()}
+                  pokeType={poke.data.types[0].type.name}
+                />
+              ))
+          : valueName || valueNumber
+          ? idPoke
+              .filter(poke => poke.data.name.includes(valueName))
+              .filter(poke => poke.data.id == valueNumber)
+              .map((poke, key) => (
+                <CardPokemon
+                  key={key}
+                  id={poke.data.id}
+                  img={poke.data.sprites.front_default}
+                  name={poke.data.name}
+                  numero={poke.data.id.toString()}
+                  pokeType={poke.data.types[0].type.name}
+                />
+              ))
+          : idPoke
+              .slice(0, pokeLimited)
+              .map((poke, key) => (
+                <CardPokemon
+                  key={key}
+                  id={poke.data.id}
+                  img={poke.data.sprites.front_default}
+                  name={poke.data.name}
+                  numero={poke.data.id.toString()}
+                  pokeType={poke.data.types[0].type.name}
+                />
+              ))}
       </div>
 
       <div className='flex justify-center items-center'>
         <button
           className='px-4 py-2 bg-yellow-300 border border-white rounded-lg'
           onClick={() => {
-            setAddPoke(addPoke + 25)
-            console.log(addPoke)
+            pokeLimited >= 150 ? setAddPoke(addPoke + 25) : ''
+            setPokeLimited(pokeLimited + 25)
           }}
         >
           Mais Pokemons...
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ export default function Index() {
   return (
     <>
       <Seo />
-      <main className='w-full h-full bg-gradient-to-b from-sky-500 via-yellow-500 to-indigo-500 '>
+      <main className='min-h-screen h-full bg-gradient-to-b from-sky-500 via-yellow-500 to-indigo-500'>
         {/* <App /> */}
         <Home />
         <PokedexList />
 
-        <footer className='h-full flex justify-center p-14 text-lg font-mono font-semibold'>
+        <footer className='flex justify-center p-14 text-lg font-mono font-semibold'>
           <p>em desenvolvimento 🚧 por Isaac S. Silva...</p>
         </footer>
       </main>
