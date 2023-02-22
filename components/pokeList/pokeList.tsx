ecf8c00a07d3de213df15ebb0481d1e2397c7500
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-02-21 23:56:43 -0300

    🎨
    🎨
      Add dica de como usar a pusca de pokemon

--- components/pokeList/pokeList.tsx
+++ components/pokeList/pokeList.tsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { CardPokemon } from './pokeCard'
 import { Header } from '../Header'
+import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline'
 
 export const PokedexList = () => {
   const [addPoke, setAddPoke] = useState<number>(150)
@@ -38,23 +39,47 @@ export const PokedexList = () => {
       .then((poke: any) => setIdPoke([...poke]))
   }
 
+  let valorInputPokedex = ''
+
+  const [isActiveTheTips, setisActiveTheTips] = useState(false)
+  const onclickActive = () => {
+    setisActiveTheTips(!isActiveTheTips)
+  }
+
   return (
     <>
       <Header />
-      <div className='m-auto flex flex-row justify-center items-center gap-2 img'>
-        <input
-          className='px-2 py-1 rounded-sm bg-opacity-75 bg-white focus:bg-opacity-100 outline-none'
-          placeholder='Pikachu ou #25'
-          type='text'
-          onChange={(e: any) => {
-            let valor =
-              e.target.value.substr(0, 1) == '#'
-                ? parseInt(e.target.value.replace(/[^0-9]/g, ''))
-                : e.target.value.toLowerCase()
+      <div className='m-auto flex flex-row justify-center items-center gap-2 '>
+        <div className='p-2 flex flex-col gap-2 border-2 border-sky-800 rounded-md'>
+          <div className='flex flex-row '>
+            <input
+              className='px-4 py-1 z-0 -right-3 relative rounded-l-lg bg-opacity-75 bg-white focus:bg-opacity-100 outline-none'
+              placeholder='Pikachu ou #25'
+              type='text'
+              onChange={(e: any) => {
+                valorInputPokedex =
+                  e.target.value.substr(0, 1) == '#'
+                    ? parseInt(e.target.value.replace(/[^0-9]/g, ''))
+                    : e.target.value.toLowerCase()
 
-            setValueName(valor)
-          }}
-        />
+                setValueName(valorInputPokedex)
+              }}
+            />
+            <span onClick={onclickActive}>
+              <QuestionMarkCircleIcon className='w-8 z-20 right-1 relative bg-yellow-400 rounded-full' />
+            </span>
+          </div>
+          <div
+            aria-hidden={isActiveTheTips}
+            className='hidden text-center m-auto w-64 p-3 bg-blue-500 bg-opacity-95 rounded-l-3xl rounded-br-3xl text-white
+            aria-hidden:flex '
+          >
+            <p>
+              Caso queira fazer a busca pelo numero da Pokedex, Use{' '}
+              <strong> # </strong> antes da propria busca
+            </p>
+          </div>
+        </div>
       </div>
 
       <div className='flex flex-row flex-wrap m-auto my-10 mx-1 md:m-5  justify-center items-center gap-5'>
