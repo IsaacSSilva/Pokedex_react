814a161348b7a8e25bab1df39939914360faee39
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-02-25 11:56:35 -0300

    🚧💄
      Em desenvolvimento a HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,68 @@
+import Link from 'next/link'
 import { Seo } from '../components/common/Seo'
-import { PokedexList } from '../components/pokeList/pokeList'
-import { HomePage } from '../components/HomePage/homePage'
+import Image from 'next/image'
+import Linkedin from '../src/assets/icons8-linkedin-circled.svg'
+import Github from '../src/assets/icons8-github.svg'
 
 export default function Index() {
   return (
     <>
-      <Seo title='PokeDex' />
-      <main className='min-h-screen h-full bg-sky-500'>
-        <PokedexList />
+      <Seo title='Home PokeDex' />
+      <main className='w-screen h-screen flex items-end md:justify-end md:items-center'>
+        <div className='flex flex-col gap-20 md:gap-7 m-3 md:w-96 text-right'>
+          <h1 className='uppercase font-bold text-5xl'>I Pokemon</h1>
+          <div>
+            <p>
+              Essa e uma lista com os pokemons inspirada em PokeDex, chamada
+              IPokeDex projetada para te trazer uma breve amostra dos pokemons.
+            </p>
+            <p>
+              As informaoes da IPokeDex foi retirada da{' '}
+              <Link
+                target='_blank'
+                href={'https://pokeapi.co/'}
+                className='font-semibold after:content-["👈"]'
+              >
+                PokeAPI
+              </Link>
+            </p>
+            <Link target='_self' href={'./pokedex'}>
+              <div className='w-24 p-2 rounded-full text-center bg-yellow-300'>
+                I PokeDex
+              </div>
+            </Link>
+          </div>
 
-        {/* <HomePage /> */}
+          <div>
+            <p>A IPokeDex Foi Desenvolvido com 💜 por Isaac S.Silva</p>
+            <ul className='flex flex-row'>
+              <li>
+                <Link target='_blank' href={'https://github.com/IsaacGSS'}>
+                  <span>
+                    <Image className='w-10' src={Github} alt='Linkedin Icone' />
+                  </span>
+                </Link>
+              </li>
 
-        <footer className='flex justify-center p-14 font-roboto'>
-          <p>em desenvolvimento 🚧 por Isaac S. Silva...</p>
-        </footer>
+              <Link
+                target='_blank'
+                href={
+                  'https://www.linkedin.com/in/isaac-gabriel-sousa-silvadev/'
+                }
+              >
+                <li>
+                  <span>
+                    <Image
+                      className='w-10'
+                      src={Linkedin}
+                      alt='Linkedin Icone'
+                    />
+                  </span>
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </div>
       </main>
     </>
   )
---
+++ pages/pokedex.tsx
@@ -0,0 +1,15 @@
+import { Seo } from '../components/common/Seo'
+import { PokedexList } from '../components/pokeList/pokeList'
+
+const Pokedex = () => {
+  return (
+    <>
+      <Seo title='PokeDex' />
+      <main className='min-h-screen h-full bg-sky-500'>
+        <PokedexList />
+      </main>
+    </>
+  )
+}
+
+export default Pokedex
