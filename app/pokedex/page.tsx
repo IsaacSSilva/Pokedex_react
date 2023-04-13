b3570f998202c8af4f35df7b0133bd80958416bf
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2023-04-12 23:32:19 -0300

    loading

--- app/pokedex/page.tsx
+++ app/pokedex/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react'
 import { Header } from './Header'
+import Loading from './Loading'
 import { PokedexList } from './pokeList'
 
 const Pokedex = () => {
   return (
     <main className='bg-sky-500'>
       <Header />
+
       <PokedexList />
     </main>
   )
