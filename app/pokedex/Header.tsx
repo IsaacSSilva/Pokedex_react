382800ad6888ebb2108e543787861f46ad7e7009
Isaac S. Silva <85599926+IsaacGSS@users.noreply.github.com>
2023-12-28 14:59:49 +0000

    new formated

--- app/pokedex/Header.tsx
+++ app/pokedex/Header.tsx
@@ -2,12 +2,15 @@ import Image from 'next/image'
 import pokeIcon from 'public/pokeicon.png'
 export const Header = () => {
   return (
-    <header>
+    <header
+    className='flex justify-end px-10 py-4'
+    >
+
       <Image
         src={pokeIcon}
         alt='icone pokemon'
         priority
-        className='w-1/2 m-auto'
+        className='w-32'
       />
     </header>
   )
--- app/pokedex/loading.tsx
+++ app/pokedex/loading.tsx
@@ -1,4 +1,4 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
-  return <>loading</>
+  return <p>loading</p>
 }
